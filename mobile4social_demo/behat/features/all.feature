1d9069a2bf69a1c326a68a215efc0cdb00f2f151
Shushu Inbar <shushu@gizra.com>
2014-04-01 21:21:34 +0300

    Try first behat/travis code.

---
+++ mobile4social_demo/behat/features/all.feature
@@ -0,0 +1,6 @@
+Feature:
+  All Mobile4Social Demo tests
+
+  @api @all
+  Scenario: Test basic installation
+    Given I visit "user"
---
+++ mobile4social_demo/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+
+use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Context\Step\Given;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Gherkin\Node\PyStringNode;
+use Guzzle\Service\Client;
+use Behat\Behat\Context\Step;
+use Behat\Behat\Context\Step\When;
+
+require 'vendor/autoload.php';
+
+class FeatureContext extends DrupalContext {
+
+}
